27d144063bf863090cdb8f453d766167081d5499
Ankur Pandey <pankur216@gmail.com>
2025-02-04 17:15:54 -0500

    opened all chapters and added them to docs array

--- submission.py
+++ submission.py
@@ -1,5 +1,36 @@
-romance_novel_1 = open("./text_data/romance/stella_rosevelt.txt")
-romance_novel_2 = open("./text_data/romance/thrice_wedded.txt")
-romance_novel_3 = open("./text_data/under_the_desert_stars.txt")
-romance_novel_4 = open("./text_data/wooing_of_leola.txt")
+import os
+import glob
 
+# Creating two different docs for the two categories
+romance_docs = []
+crime_docs = []
+
+def open_txt_files_in_directory(directory, array):
+    # Construct the search pattern to match all .txt files in the directory
+    search_pattern = os.path.join(directory, '*.txt')
+    
+    # Use glob to find all .txt files in the directory
+    txt_files = glob.glob(search_pattern)
+    
+    # Loop through each .txt file and open it
+    for txt_file in txt_files:
+        try:
+            with open(txt_file, 'r', encoding='utf-8') as file:
+                contents = file.read()
+                array.append(contents)
+
+        except Exception as e:
+            print(f"Error opening file {txt_file}: {e}")
+
+# Opening all chapters for each romance based novel
+open_txt_files_in_directory('./text_data/romance/stella_rosevelt', romance_docs)
+open_txt_files_in_directory('./text_data/romance/thrice_wedded', romance_docs)
+open_txt_files_in_directory('./text_data/romance/under_the_desert_stars', romance_docs)
+open_txt_files_in_directory('./text_data/romance/wooing_of_leola', romance_docs)
+
+# Opening all chapters for each crime based novel
+open_txt_files_in_directory('./text_data/romance/black_twenty_two', crime_docs)
+open_txt_files_in_directory('./text_data/romance/draycott', crime_docs)
+open_txt_files_in_directory('./text_data/romance/hound_of_baskerville', crime_docs)
+open_txt_files_in_directory('./text_data/romance/murder_on_the_links', crime_docs)
+open_txt_files_in_directory('./text_data/romance/sherlock_holmes', crime_docs)
