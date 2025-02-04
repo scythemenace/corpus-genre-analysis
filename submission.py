c9384863b33cbb661a2f56850de30f5dc32eaa51
Ankur Pandey <pankur216@gmail.com>
2025-02-04 14:52:31 -0500

    divided black_twenty into chapters

---
+++ submission.py
@@ -0,0 +1,5 @@
+romance_novel_1 = open("./text_data/romance/stella_rosevelt.txt")
+romance_novel_2 = open("./text_data/romance/thrice_wedded.txt")
+romance_novel_3 = open("./text_data/under_the_desert_stars.txt")
+romance_novel_4 = open("./text_data/wooing_of_leola.txt")
+
